335fad60f95514ae3a2f0783a61b453305e4be64
Niko Kozak <nikokozak@gmail.com>
2025-02-17 16:34:26 -0500

    draws grid, draws cursor, activates and deactivates

--- sketch.js
+++ sketch.js
@@ -23,7 +23,17 @@ const SAMPLES = [
   'TR-505_Tape_Snare.wav'
 ]
 
-const STATE = {
+// ************************************************************************************************************************************************************************************
+//                              STATE SETUP
+// ************************************************************************************************************************************************************************************
+
+let STATE = {
+  cursor: {
+    mode: 'arrangement',
+    arrangement: 0,
+    track: 0,
+    trigger: 0
+  },
   transport: {
     isPlaying: false,
     bpm: BPM,
@@ -62,6 +72,49 @@ function makeTriggers() {
   });
 }
 
+// ************************************************************************************************************************************************************************************
+//                              ACTIONS
+// ************************************************************************************************************************************************************************************
+
+function moveCursor(direction, state) {
+  return produce(state, draft => {
+    switch (direction) {
+      case 'down':
+        if (draft.cursor.mode === 'arrangement') {
+          draft.cursor.track = draft.cursor.track === 0 ? draft.arrangements[draft.cursor.arrangement].tracks.length - 1 : draft.cursor.track - 1;
+        }
+        break;
+      case 'up':
+        if (draft.cursor.mode === 'arrangement') {
+          draft.cursor.track = (draft.cursor.track + 1) % draft.arrangements[draft.cursor.arrangement].tracks.length;
+        }
+        break;
+      case 'left':
+        if (draft.cursor.mode === 'arrangement') {
+          draft.cursor.trigger = draft.cursor.trigger === 0 ? BARS * TIME_SIGNATURE - 1 : draft.cursor.trigger - 1;
+        }
+        break;
+      case 'right':
+        if (draft.cursor.mode === 'arrangement') {
+          draft.cursor.trigger = (draft.cursor.trigger + 1) % (BARS * TIME_SIGNATURE);
+        }
+        break;
+    }
+  })
+}
+
+// Turns "on" the trigger at the current cursor position
+function selectTrigger(state) {
+  return produce(state, draft => {
+    const isActive = draft.arrangements[draft.cursor.arrangement].tracks[draft.cursor.track].triggers[draft.cursor.trigger].isActive;
+    draft.arrangements[draft.cursor.arrangement].tracks[draft.cursor.track].triggers[draft.cursor.trigger].isActive = !isActive;
+  });
+}
+
+// ************************************************************************************************************************************************************************************
+//                              THREE.JS SETUP
+// ************************************************************************************************************************************************************************************
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(100, 100, 200);
@@ -88,39 +141,45 @@ function animate() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   controls.update();
+  drawState(STATE);
   renderer.render(scene, camera);
 }
 renderer.setAnimationLoop(animate);
+
+
+// ************************************************************************************************************************************************************************************
+//                              DRAWING FUNCTIONS
+// ************************************************************************************************************************************************************************************
+
 
 function drawState(state) {
   // Draw the transport
   // drawTransport(state.transport);
   // Draw the arrangements
-  console.log(state);
   drawArrangements(state.arrangements);
 }
 
 function drawArrangements(arrangements) {
-  arrangements.forEach(arrangement => {
+  arrangements.forEach((arrangement, arrangementIdx) => {
     if (arrangement.isSelected) {
-      drawTracks(arrangement.tracks);
+      drawTracks(arrangement.tracks, arrangementIdx);
     }
   });
 }
 
-function drawTracks(tracks) {
+function drawTracks(tracks, arrangementIdx) {
   tracks.forEach((track, trackIdx) => {
-    drawTrack(track, trackIdx);
+    drawTrack(track, trackIdx, arrangementIdx);
   });
 }
 
-function drawTrack(track, trackIdx) {
-  drawTrackLine(track.triggers, trackIdx);
+function drawTrack(track, trackIdx, arrangementIdx) {
+  drawTrackLine(track.triggers, trackIdx, arrangementIdx);
   // Draw the triggers
-  drawTriggers(track.triggers, trackIdx);
+  drawTriggers(track.triggers, trackIdx, arrangementIdx);
 }
 
-function drawTrackLine(triggers, trackIdx) {
+function drawTrackLine(triggers, trackIdx, arrangementIdx) {
   const points = [];
   for (let i = 0; i < triggers.length; i++) {
     points.push(new THREE.Vector3(
@@ -134,134 +193,20 @@ function drawTrackLine(triggers, trackIdx) {
   scene.add(line);
 }
 
-function drawTriggers(triggers, trackIdx) {
-  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+function drawTriggers(triggers, trackIdx, arrangementIdx) {
+  const onMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const offMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const selectedMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
   const geometry = new THREE.SphereGeometry(5, 32, 32);
   for (let i = 0; i < triggers.length; i++) {
-    const sphere = new THREE.Mesh(geometry, material);
+    const isSelected = STATE.cursor.mode === 'arrangement' && STATE.cursor.arrangement === arrangementIdx && STATE.cursor.track === trackIdx && STATE.cursor.trigger === i;
+    const sphere = new THREE.Mesh(geometry, triggers[i].isActive ? onMaterial : isSelected ? selectedMaterial : offMaterial);
     sphere.position.set(GRID_DRAW_X_ORIGIN + trackIdx * GRID_TRIGGER_SPACING, GRID_DRAW_Y_ORIGIN, GRID_DRAW_Z_ORIGIN + i * GRID_TRIGGER_SPACING);
     scene.add(sphere);
   }
 }
 
 drawState(STATE);
-
-
-// ************************************************************************************************************************************************************************************
-//                                                       METAGRID
-// ************************************************************************************************************************************************************************************
-
-// Create a data structure to represent the meta-grid, which will dictate where we can draw UI elements, and consequently where we can trigger samples.
-// Think of this as a sub-sampling of the draw-space of the sketch; Given that we're only really interested in drawing elements at very specific and regular
-// points in space, we don't need to worry about the entire canvas. The meta-grid therefore depends on the resolution of the sketch, and our preferred spacing,
-// and extends in the z-axis so as to create a cube.
-function createMetaGrid(sketchWidth, sketchHeight, spacing) {
-  // There is also a "padding" equal to the spacing all around the grid.
-  const padding = spacing;
-  const gridWidth = sketchWidth - 2 * padding;
-  const gridHeight = sketchHeight - 2 * padding;
-  const gridDepth = sketchHeight - 2 * padding;
-  const grid =
-  {
-    points: [], // The x-y coordinates of each point in the grid.
-    width: Math.floor((gridWidth - padding) / spacing), // The width of the grid in terms of the number of points.
-    height: Math.floor((gridHeight - padding) / spacing), // The height of the grid in terms of the number of points. 
-    depth: Math.floor((gridDepth - padding) / spacing) // The depth of the grid in terms of the number of points.
-  };
-
-  // Fetch a point in a 1-dimensional array by its x, y, and z coordinates.
-  function getPoint(x, y, z) {
-    return grid.points[x + y * grid.width + z * grid.width * grid.height];
-  }
-
-  // Populate the metagrid
-  for (let x = padding; x < gridWidth; x += spacing) {
-    for (let y = padding; y < gridHeight; y += spacing) {
-      for (let z = padding; z < gridDepth; z += spacing) {
-        grid.points.push([x, y, z]);
-      }
-    }
-  }
-  return grid;
-}
-
-// Returns the x-y coordinates of a given x-y point in the grid.
-function getMetaGridPointCoords(x, y, z, metagrid) {
-  // return the coordinate using width, height, and depth in the metagrid object
-  return metagrid.points[x + metagrid.width * (y + metagrid.depth * z)]
-}
-
-// Returns a plane of points in the metagrid that lie on a given axis and at a given origin point.
-// Pass in 'xy' or 'xz' or 'yz' for the axis, and the origin point.
-function getMetaGridPlane(axis, originPoint, metagrid) {
-  const plane = [];
-  axis = axis === 'xy' ? 2 : axis === 'xz' ? 1 : 0;
-  for (let i = 0; i < metagrid.width; i++) {
-    for (let j = 0; j < metagrid.height; j++) {
-      const coords = getMetaGridPointCoords(i, j, metagrid);
-      if (coords[axis] === originPoint[axis]) {
-        plane.push(coords);
-      }
-    }
-  }
-  return plane;
-}
-
-// Returns a set of points representing a line in the metagrid space.
-// Pass in 'x' or 'y' or 'z' for the axis, and the origin point.
-function getMetaGridLine(axis, originPoint, metagrid) {
-  const line = [];
-  axis = axis === 'x' ? 0 : axis === 'y' ? 1 : 2;
-  for (let i = 0; i < metagrid.width; i++) {
-    for (let j = 0; j < metagrid.height; j++) {
-      const coords = getMetaGridPointCoords(i, j, metagrid);
-      switch (axis) {
-        case 'x':
-          if (coords[1] == originPoint[1] && coords[2] == originPoint[2]) { line.push(coords); }
-          break;
-        case 'y':
-          if (coords[0] == originPoint[0] && coords[2] == originPoint[2]) { line.push(coords); }
-          break;
-        case 'z':
-          if (coords[0] == originPoint[0] && coords[1] == originPoint[1]) { line.push(coords); }
-          break;
-      }
-    }
-  }
-  return line;
-}
-
-// Draw the trigger grid using the metagrid as the drawspace
-function drawTriggerGrid(triggers, posX, posY, posZ, metagrid) {
-  // Draw trigger row for each sample
-  for (let i = 0; i < Object.keys(triggers).length; i++) {
-    drawTriggerRow(triggers[Object.keys(triggers)[i]], posX, posY, posZ, metagrid);
-  }
-}
-
-// Draw trigger row for each sample
-function drawTriggerRow(triggerRow, posX, posY, posZ, metagrid) {
-  if (BARS * TIME_SIGNATURE !== triggerRow.length) { throw new Error('Trigger length mismatch'); }
-
-  for (let i = 0; i < triggerRow.length; i++) {
-    drawTriggerDot(posX + i, posY, posZ, metagrid, triggerRow[i]);
-  }
-}
-
-// Draw a trigger grid dot
-function drawTriggerDot(posX, posY, posZ, triggerVal, metagrid) {
-  const metaGridPoint = getMetaGridPointCoords(posX, posY, posZ, metagrid);
-  if (triggerVal == 1) { // If trigger is on
-    // Draw a black circle
-    fill(GRID_DOT_COLOR);
-    circle(metaGridPoint[0], metaGridPoint[1], GRID_DOT_RADIUS);
-  } else { // If trigger is off
-    // Draw a white circle with a black outline
-    stroke(GRID_DOT_COLOR);
-    fill('white');
-    circle(metaGridPoint[0], metaGridPoint[1], GRID_DOT_RADIUS);
-  }
-}
 
 // ************************************************************************************************************************************************************************************
 //                                                      TONE.JS 
@@ -318,12 +263,45 @@ function draw() {
 
 }
 
-// If spacebar is pressed, start the transport and play
-function keyPressed() {
-  if (keyCode === 32) {
+// Replace keyPressed function with event listener
+document.addEventListener('keydown', (event) => {
+
+  if (event.code === 'Space') {
     transport.start();
     console.log('Transport started');
   }
-}
+
+  if (event.code == "KeyA") {
+    event.preventDefault();
+    STATE = selectTrigger(STATE);
+  }
+
+  switch (event.code) {
+    case 'ArrowLeft':
+      event.preventDefault();
+      STATE = moveCursor('left', STATE);
+      console.log('Cursor moved left:', STATE.cursor);
+      break;
+    case 'ArrowUp':
+      event.preventDefault();
+      STATE = moveCursor('up', STATE);
+      console.log('Cursor moved up:', STATE.cursor);
+      break;
+    case 'ArrowRight':
+      event.preventDefault();
+      STATE = moveCursor('right', STATE);
+      console.log('Cursor moved right:', STATE.cursor);
+      break;
+    case 'ArrowDown':
+      event.preventDefault();
+      STATE = moveCursor('down', STATE);
+      console.log('Cursor moved down:', STATE.cursor);
+      break;
+  }
+
+  // Clear and redraw scene after cursor moves
+  scene.clear();
+  drawState(STATE);
+});
 
 
